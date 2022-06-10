83819bff5b2df59ccc254765b5a5a1febd81b1b6
DoomASK <106534531+DoomASK@users.noreply.github.com>
2022-06-10 12:41:33 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -1,13 +1,10 @@
 'use strict';
-/**
- * возвращает дробную часть х
- * @param {nuber} x число
- * @returns number
- */
+
 function getDecimal(num) {
-    let a = num.toString();
-    a = a.slice(a.indexOf("."));
-    
+    let a = num.slice(num.indexOf('.'));
+    if (num.indexOf(".") == -1) {
+        return 0;
+    }
     let str = 0 + a;
     if (Number(num) < 0) {
         str = 1 - Number(str);
@@ -15,106 +12,96 @@ function getDecimal(num) {
     }
     return str;
 }
-/**
- * возвращает строку с заглавным первым символом
- * @param {string} str строка
- * @returns string
- */
+
 function ucFirst(str) {
-    return (str[0].toUpperCase() + str.slice(1));
+    if (!str) return str;
+
+    return str[0].toUpperCase() + str.slice(1);
 }
-/**
- * проверяет на наличие ххх и viagra
- * @param {string} str строка
- * @returns bool
- */
+
 function checkSpam(str) {
-    str = str.toLowerCase();
-    if ((str.includes('xxx')) || (str.includes('viagra')))
+    let firstWord = 'viagra';
+    let secondWord = 'xxx';
+    let string = str.toLowerCase();
+    let result;
+
+    if (checkTheWord(firstWord) || checkTheWord(secondWord)) {
         return true;
-    else return false;
+    }
+    return false;
+
+    function checkTheWord(word) {
+        for (let i = 0; i < string.length - 1; i++) {
+            if (string[i] == word[0]) {
+                result = string.slice(i, i + word.length);
+                if (result == word) {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+    }
 }
-/**
- * сокращает строчку до макс знаков, ставит ... в конце
- * @param {string} str строка
- * @param {number} maxlength длинна строки
- * @returns bool
- */
+
+
 function truncate(str, maxlength) {
     if (str.length > maxlength)
         str = str.slice(0, maxlength - 1) + '\u{2026}';
     return str;
 }
-/**
- * возвращает строку без -
- * @param {string} str строка
- * @returns string
- */
+
+
 function camelize(str) {
-    let id;
-    while (str.includes("-")) {
-        id = str.indexOf('-');
-        str = str.slice(0, id) + str[id + 1].toUpperCase() + str.substring(id + 2);
-    }
-    return str;
+    return str
+        .split('-')
+        .map(
+            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        )
+        .join('');
 }
 
-/**
- * возвращает массив заполненый числами фиббоначи до n-1'го номера
- * @param {number} N номер числа фиббоначи
- * @returns string
- */
- function fibs(N) {
-     /**
- * возвращает число последовательности фиббоначи
- * @param {number} p номер числа фиббоначи
- * @returns number
- */
- function fib(p){
-        let a = 0n;
-        let b = 1n;
-        //Исключения
-        if (p==0) return a;
-        if (p==1) return b;
-        for (let i = 2; i <= p; i++) {
-          let c = a + b;
-          a = b;
-          b = c;
-        }
-        return b;
-      }
+function fibs(N) {
     let a = [];
+    let str = '';
     if (!Number.isNaN(N))
-            for (let p = 0; p < N; ++p) {
-                a[p] = fib(p);
+        if (N == 0) str = 0;
+        else
+            for (let j = 0; j < N; ++j) {
+                a[j] = fib(j);
+                str += a[j] + ' ';
             }
-    else return NaN;
- 
-    return a;
- }
-/**
- * возвращает reverse массив
- * @param {array} arr неупорядочный массив чисел
- * @returns array
- */
-function arrReverseSorted(arr){
-let arr_reserve=[];
+    else str = NaN;
 
-arr_reserve=arr;
-    arr_reserve.sort((a, b) => b - a);
+    return str;
 
-alert(arr_reserve);
+    function fib(j) {
+        let a = 1n;
+        let b = 1n;
+        let c;
+        if (j == 0) return 0;
+        if (j == 1) return 1;
+        if (j == 2) return 1;
+        for (let i = 3; i <= j; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        if (b == 1n)
+            b = 0;
+        return b;
+    }
 }
-/**
- * возвращает сумму всех аргументов
- * @returns number
- */
+
+
+
+
 function sum() {
     let result = 0;
-  
+
     for (let i = 0; i < arguments.length; i++) {
-      result += arguments[i];
+        result += arguments[i];
     }
-  
+
     return result;
-  }
+}
