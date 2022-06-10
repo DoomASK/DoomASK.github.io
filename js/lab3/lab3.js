3f21c9d36aa8637957603a9232f39b34e6c19cc3
DoomASK <106534531+DoomASK@users.noreply.github.com>
2022-06-10 12:32:34 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -97,12 +97,13 @@ function camelize(str) {
  * @param {array} arr неупорядочный массив чисел
  * @returns array
  */
-function arrReverseSorted(arr) {
-    let arr_reserve = [];
+function arrReverseSorted(arr){
+let arr_reserve=[];
 
-    arr_reserve = arr.sort((a, b) => b - a);
+arr_reserve=arr;
+    arr_reserve.sort((a, b) => b - a);
 
-    return(arr_reserve);
+alert(arr_reserve);
 }
 /**
  * возвращает сумму всех аргументов
